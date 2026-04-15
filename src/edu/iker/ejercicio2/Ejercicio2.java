97c1ccdb17f4bc23a01a3686b8e737634220a641
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2026-04-15 09:54:54 +0200

    Ejercicio 2 terminado

--- src/edu/iker/ejercicio2/Ejercicio2.java
+++ src/edu/iker/ejercicio2/Ejercicio2.java
@@ -1,5 +1,33 @@
 package edu.iker.ejercicio2;
 
+import java.util.ArrayList;
+
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(10);
+        numeros.add(4);
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(2);
+
+        System.out.println("Números de la lista:");
+        int numerosTotales=0;
+        int sumaNumeros=0;
+        int numeroMayor=numeros.get(0);
+        for (Integer numero : numeros) {
+            System.out.println(numero);
+            if (numero > numeroMayor) {
+                numeroMayor = numero;
+            }
+            numerosTotales++;
+            sumaNumeros+=numero;
+        }
+        System.out.println(numerosTotales);
+        System.out.println(sumaNumeros);
+        System.out.println(numeroMayor);
+
+
+    }
 
 }
